4e511bc1ed35d42cdbfff3ef79ef2c7678e00e47
FaezeGhanbari <f.ghanbari@tazarv.com>
2024-07-24 16:47:33 +0330

    Design UI and Calculate amount

--- FinancialFramework/MainWindow.xaml.cs
+++ FinancialFramework/MainWindow.xaml.cs
@@ -20,9 +20,32 @@ namespace FinancialFramework
     /// </summary>
     public partial class MainWindow : ThemedWindow
     {
+        private int _amount;
+        private int _twentyPercent;
+        private int _thirtyPercent;
+        private int _fiftyPercent;
         public MainWindow()
         {
             InitializeComponent();
+        }
+        
+        private void txtCalculate_Click(object sender, RoutedEventArgs e)
+        {
+            _amount = Convert.ToInt32(txtAmount.Text);
+            _calculateAmount(_amount);
+            _fillUI();
+        }
+        private void _calculateAmount(int amount)
+        {
+            _twentyPercent = (amount / 100) * 20;
+            _thirtyPercent = (amount / 100) * 30;
+            _fiftyPercent = (amount / 100) * 50;
+        }
+        private void _fillUI()
+        {
+            txt20Percent.Text= _twentyPercent.ToString();
+            txt30Percent.Text= _thirtyPercent.ToString();
+            txt50Percent.Text= _fiftyPercent.ToString();
         }
     }
 }
